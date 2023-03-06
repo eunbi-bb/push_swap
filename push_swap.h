ebada83736b8af084c2ecd99f184289d4b112b58
Eunbi Cho <eucho@f0r1s6.codam.nl>
2023-03-06 08:56:27 +0100

    arguments are stacked in stack as an integer

--- push_swap.h
+++ push_swap.h
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include "libft/libft.h"
+#include <stdio.h>
 
 typedef struct s_node
 {
--- src/push_swap/push_swap.c
+++ src/push_swap/push_swap.c
@@ -5,21 +5,28 @@ void	push_swap(char **argv)
 	t_stack	*stack;
 	int		size;
 	int		i;
+	int		tmp;
 
 	i = 0;
 	size = pusw_strlen(argv);
-	stack = malloc(sizeof(t_node));
+	stack = malloc(sizeof(t_stack));
+	if (!stack)
+		return ;
+	stack->a = NULL;
 	while (i < size)
 	{
-		add_after(stack->a, new_node(pusw_atoi(argv[i], stack->a)));
-		i++;
+		tmp = pusw_atoi(argv[i], stack->a);
+		if (!stack->a)
+			stack->a = new_node(tmp);
+		else
+			add_after(stack->a, new_node(tmp));
+		i++;  
 		stack->a = stack->a->next;
 	}
 	stack->size_a = size;
 	duplication_check(stack->a);
 	sort(stack, size);
 	free(stack->a);
-	free(stack->b);
 }
 
 int	main(int argc, char **argv)
